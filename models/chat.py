ef5e1d4952c482408ee0d2d614d41ded2f10afa8
Modeste74 <ciiramodeste23@gmail.com>
2024-02-29 21:22:32 +0300

    created a class that will facilitate the hub chats

---
+++ models/chat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines a class Chat"""
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
+class Chat(BaseModel, Base):
+    """Represents a group chat in a hub."""
+    __tablename__ = 'chats'
+
+    hub_id = Column(String(60), ForeignKey('hubs.id'), nullable=False)
+    sender_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    message = Column(String(1024), nullable=False)
+
+    hub = relationship("Hub", back_populates="chats")
+    sender = relationship("User", foreign_keys=[sender_id])
