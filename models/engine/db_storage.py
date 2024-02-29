1161c1f60b7840934b93cf94a879623d515b6630
Modeste74 <ciiramodeste23@gmail.com>
2024-02-29 21:17:17 +0300

    added a few more classes in the storage system

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,11 +4,18 @@ import models
 from models.base_model import BaseModel, Base
 from models.messages import Message
 from models.users import Users
+from models.hub import Hub
+from models.resource import Resource
+from models.chat import Chat
+from models.group import Group
+from models.group_members import GroupMember
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from os import getenv
 
-classes = {"Users": Users, "Message": Message}
+classes = {"Users": Users, "Message": Message, "Hub": Hub,
+        "Resource": Resource, "Chat": Chat, "Group": Group,
+        "GroupMember": GroupMember}
 
 
 class DBStorage:
