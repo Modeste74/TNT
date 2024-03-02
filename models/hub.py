4d9a8014f6f382b9a5a6b86343c8b9e502fe90eb
Modeste74 <ciiramodeste23@gmail.com>
2024-03-02 15:57:11 +0300

    added a class HubLearners and created a new relationship

--- models/hub.py
+++ models/hub.py
@@ -12,7 +12,21 @@ class Hub(BaseModel, Base):
     name = Column(String(128), nullable=False)
     participants = Column(Integer, default=1)
 
-    tutor = relationship("User", foreign_keys=[tutor_id])
+    tutor = relationship("Users", foreign_keys=[tutor_id])
     resources = relationship("Resource", back_populates="hub")
     chats = relationship("Chat", back_populates="hub")
-    group = relationship("Group", back_populates="hub")
+    groups = relationship("Group", back_populates="hub")
+    learners = relationship("Users", secondary="hub_learners", back_populates="hubs", overlaps="hubs_association")
+    learners_association = relationship("HubLearners", back_populates="hub", overlaps="hubs,learners")
+
+
+class HubLearners(BaseModel, Base):
+    """defines a class for learners in hubs"""
+    __tablename__ = 'hub_learners'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    hub_id = Column(String(60), ForeignKey('hubs.id'), nullable=False)
+    learner_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    hub = relationship("Hub", back_populates="learners_association", overlaps="hubs,learners")
+    learner = relationship("Users", back_populates="hubs_association", overlaps="hubs,learners")
