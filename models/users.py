354456ffaceb4cb7e25887b1e85f4c502380bd51
Modeste74 <ciiramodeste23@gmail.com>
2024-03-02 16:01:50 +0300

    added usermixin

--- models/users.py
+++ models/users.py
@@ -5,15 +5,16 @@ from sqlalchemy import Column, String, Enum
 from sqlalchemy.orm import relationship
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from models.messages import Message
 
 
-class Users(BaseModel, Base):
+class Users(UserMixin, BaseModel, Base):
     """defines class and it table"""
     __tablename__ = 'users'
     id = Column(String(60), primary_key=True)
     password = Column(String(500), nullable=False)
-    username = Column(String(128), nullable=False)
+    username = Column(String(128), nullable=False, unique=True)
     user_type = Column(Enum('tutor', 'learner'), nullable=False)
 
     def __init__(self, *args, **kwargs):
@@ -33,3 +34,5 @@ class Users(BaseModel, Base):
     
     sent_messages = relationship("Message", foreign_keys=[Message.sender_id], back_populates="sender")
     received_messages = relationship("Message", foreign_keys=[Message.receiver_id], back_populates="receiver")
+    hubs = relationship("Hub", secondary="hub_learners", back_populates="learners")
+    hubs_association = relationship("HubLearners", back_populates="learner", overlaps="hubs")
