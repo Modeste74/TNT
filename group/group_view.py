3c20a3107a52b58693514fa48810685109485e25
Modeste74 <ciiramodeste23@gmail.com>
2024-03-05 14:48:45 +0300

    added and enpoint for creating and displaying group chats for different groups

--- group/group_view.py
+++ group/group_view.py
@@ -3,10 +3,11 @@
 from . import group_bp
 from flask import Flask, request, render_template, redirect
 from flask import flash, url_for
+from flask_login import current_user
 from models import storage
 from models.users import Users
 from models.hub import Hub
-from models.group import Group
+from models.group import Group, GroupMessage
 from models.group_members import GroupMember
 import os
 
@@ -22,34 +23,71 @@ def create_group(hub_id):
 		new_group = Group(hub_id=hub_id, name=group_name)
 		storage.new(new_group)
 		storage.save()
+		groups = storage.all(Group).values()
+		group = next((grp for grp in groups
+			if grp.name == group_name))
+		add_tutor = GroupMember(user_id=current_user.id,
+			group_id=group.id)
+		storage.new(add_tutor)
+		storage.save()
 		flash(f"Group '{group_name}' created successfully",
 			"success")
 		return redirect(url_for('hub.hub_display', hub_id=hub.id))
 	return render_template('create_group.html', hub=hub)
 
 
-@group_bp.route('/add_participant_to_group', methods=['GET', 'POST'])
-def add_participant_to_group():
+@group_bp.route('/add_participant_to_group/<group_id>/<hub_id>',
+	methods=['GET', 'POST'])
+def add_participant_to_group(group_id, hub_id):
 	if request.method == 'POST':
-		user_id = request.form.get('user_id')
-		group_id = request.form.get('group_id')
+		username = request.form.get('username')
 
-		user =storage.get(Users, user_id)
-		group = storage.get(Group, group_id)
-		if not user or not group:
-			flash("User of group not found", "error")
-			return redirect(url_for('group.add_participant_to_group'))
-		group_member = GroupMember(user_id=user_id, group_id=group_id)
+		users =storage.all(Users).values()
+		user = next((usr for usr in users
+			if usr.username == username))
+
+		if not user:
+			flash("User not found", "error")
+			return redirect(url_for('group.add_participant_to_group',
+				group_id=group_id, hub_id=hub_id))
+		group_member = GroupMember(user_id=user.id, group_id=group_id)
 		storage.new(group_member)
 		storage.save()
 		flash("Participant added to the group successfully",
 			"success")
-		return redirect(url_for('group.add_participant_to_group'))
-	return render_template('add_participant_to_group.html')
+		return redirect(url_for('group.add_participant_to_group',
+			group_id=group_id, hub_id=hub_id))
+	return render_template('add_participant_to_group.html',
+		group_id=group_id, hub_id=hub_id)
 
 
-@group_bp.route('/delete_group/<group_id>', methods=['GET', 'POST'])
-def delete_group(group_id):
+@group_bp.route('/group_chat/<group_id>/<sender_id>', methods=['GET', 'POST'])
+def group_chat(group_id, sender_id):
+    group = storage.get(Group, group_id)
+    sender = storage.get(Users, sender_id)
+
+    if not group or not sender:
+        return jsonify({"error": "Group or sender not found"})
+
+    if request.method == 'POST':
+        content = request.form.get('content')
+
+        new_message = GroupMessage(group_id=group_id,
+        	sender_id=sender_id, content=content)
+        storage.new(new_message)
+        storage.save()
+
+    messages = storage.all(GroupMessage).values()
+    group_messages = [message for message in messages if message.group_id == group_id]
+
+    return render_template('group_chat.html', group=group,
+    	sender=sender, group_messages=group_messages)
+
+
+
+
+@group_bp.route('/delete_group/<group_id>/<hub_id>', methods=['GET', 'POST'])
+def delete_group(group_id, hub_id):
 	group = storage.get(Group, group_id)
 	if group:
 		storage.delete(group)
@@ -58,4 +96,4 @@ def delete_group(group_id):
 			"success")
 	else:
 		flash("Group not found", "error")
-	return redirect(url_for('group.create_group'))
+	return redirect(url_for('group.create_group', hub_id=hub_id))
